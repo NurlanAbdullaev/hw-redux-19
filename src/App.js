b777a38a12399f005ccd2821d091360dff1e20c4
NurlanAbdllaev <nurlan2001002@gmail.com>
2023-05-23 19:44:58 +0600

    first commit

---
+++ src/App.js
@@ -0,0 +1,22 @@
+import { Navigate, Route, Routes } from "react-router-dom";
+import "./App.css";
+import { AuthPages } from "./pages/AuthPages";
+import { TodoPages } from "./pages/TodoPages";
+import TodoForm from "./components/todoForm/TodoForm";
+
+function App() {
+  return (
+    <div className="App">
+      <Routes>
+        <Route path="/login" element={<AuthPages />} />
+        <Route path="/todos" element={<TodoPages />}>
+          <Route path="todo" element={<TodoForm />} />
+        </Route>
+
+        <Route path="*" element={<Navigate to="login" />} />
+      </Routes>
+    </div>
+  );
+}
+
+export default App;
---
+++ src/components/header/Header.jsx
@@ -0,0 +1,50 @@
+import React from "react";
+import styled from "styled-components";
+import { authActionTypes } from "../../store/auth/authReducer";
+import { todoActionTypes } from "../../store/todo/todoReducer";
+import { useDispatch } from "react-redux";
+import { NavLink, useNavigate } from "react-router-dom";
+
+const Header = () => {
+  const dispatch = useDispatch();
+  const navigate = useNavigate();
+  const logoutHandler = () => {
+    dispatch({ type: authActionTypes.LOGOUT });
+    dispatch({ type: todoActionTypes.DELETE_ALL_TODO });
+    navigate("/login");
+  };
+  return (
+    <StyledHeader>
+      <div>
+        <NavLinkStyle to={"todo"}>Todoshka</NavLinkStyle>
+      </div>
+      <Button onClick={logoutHandler}>logout</Button>
+    </StyledHeader>
+  );
+};
+
+export default Header;
+
+const StyledHeader = styled.header`
+  height: 100px;
+  padding: 20px;
+  display: flex;
+  align-items: center;
+  justify-content: flex-end;
+  background-color: pink;
+`;
+const Button = styled.button`
+  width: 120px;
+  height: 38px;
+  border-radius: 5px;
+  background-color: black;
+  color: white;
+  border-color: #4242a2c1;
+`;
+
+const NavLinkStyle = styled(NavLink)`
+  color: black;
+  margin-right: 20px;
+  font-size: 24px;
+  text-decoration: none;
+`;
